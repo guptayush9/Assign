b2754b027d6e59558cf371137be7a7b5a870c697
guptayush9 <guptayush9@gmail.com>
2021-02-03 16:13:26 +0530

    Update authenticate-controller.js

--- controllers/authenticate-controller.js
+++ controllers/authenticate-controller.js
@@ -3,7 +3,6 @@ var connection = require('./../config');
 module.exports.authenticate=function(req,res){
     var hs_useremail=req.body.hs_useremail;
     var hs_userpassword=req.body.hs_userpassword;
-    // var query='SELECT * FROM client WHERE cl_emailid="'+email+'" and cl_password = "'+password+'"';
     var query='SELECT hs_id, hs_emailid, hs_name, hs_description, hs_address, hs_googlelocation, hs_latitude, hs_longitude, hs_registrationstatus, lc_locationpin FROM abcd WHERE hs_emailid =  "'+hs_useremail+'" AND hs_password =  "'+hs_userpassword+'"';
     console.log(query);
     connection.query(query, function (error, results, fields) {
